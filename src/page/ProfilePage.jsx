3bec12aacc7bac4773ff5978fddc7a714ee491ac
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-03-22 15:06:32 +0600

    jwt-auth-getprofiledata

--- src/page/ProfilePage.jsx
+++ src/page/ProfilePage.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import useAxios from "../hooks/useAxios";
 
 const ProfilePage = () => {
-  return <div>Profile page</div>;
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const { api } = useAxios();
+  const { auth } = useAuth();
+
+  // profile get here
+  useEffect(() => {
+    setLoading(true);
+    const fetchProfile = async () => {
+      try {
+        const response = await api.get(
+          `http://localhost:3000/profile/${auth?.user?.id}`
+        );
+        setUser(response?.data?.user);
+        setPosts(response?.data?.posts);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  // loading here
+  if (loading) {
+    return <div> Fetching your Profile data...</div>;
+  }
+  return (
+    <div>
+      Welcome, {user?.firstName} {user?.lastName}
+      <p>You have {posts.length} posts.</p>
+    </div>
+  );
 };
 
 export default ProfilePage;
